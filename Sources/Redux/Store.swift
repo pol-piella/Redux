bb20672b60dafb2ab06dbf86493eaa4270f4bcb6
Pol Piella Abadia <hi@polpiella.dev>
2021-12-20 15:30:07 +0000

    Makes queue-injection internal only

--- Sources/Redux/Store.swift
+++ Sources/Redux/Store.swift
@@ -6,17 +6,19 @@ public class Store<State, Action> {
     private let reducer: Reducer<State, Action>
     private let queue: DispatchQueueType
     
-    init(
-        initialState: State,
-        queue: DispatchQueueType = DispatchQueue(label: "com.polpiellacode.redux.store", qos: .userInitiated),
-        reducer: @escaping Reducer<State, Action>
-    ) {
+    public init(initialState: State, reducer: @escaping Reducer<State, Action>) {
+        self.currentState = initialState
+        self.reducer = reducer
+        self.queue = DispatchQueue(label: "com.polpiellacode.redux.store", qos: .userInitiated)
+    }
+    
+    init(initialState: State, queue: DispatchQueueType, reducer: @escaping Reducer<State, Action>) {
         self.currentState = initialState
         self.reducer = reducer
         self.queue = queue
     }
     
-    func dispatch(_ action: Action) {
+    public func dispatch(_ action: Action) {
         queue.sync {
             if let sideEffect = reducer(&currentState, action) {
                 queue.async(group: nil, qos: .unspecified, flags: []) { self.dispatch(sideEffect) }
